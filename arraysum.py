33a308a866628596feff3e6733878c1507ef8010
Riza Mohamed <90960705+CODEWITHRIZA@users.noreply.github.com>
2023-11-10 14:48:34 +0530

    Update arraysum.py

--- arraysum.py
+++ arraysum.py
@@ -1,3 +1,11 @@
+#Hacker solution of Simple Array Sum By Riza Mohamed T
+#Here is the link to the problem :- [ClickHere](https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true)
+# Get the count of numbers from the user and store it in 'n'.
 n = int(input().strip())
+
+# Get a list of numbers from the user, where each number is separated by a space.
+# Convert each of these numbers to integers and store them in a list called 'arr'.
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+# Calculate and print the sum of all the numbers in the list 'arr'.
 print(sum(arr))
